0f1fadebb1cd0e77d0095de70c2d3b2fc795ea1b
Winston Riggs <wriggs12@gmail.com>
2023-07-17 23:00:53 -0400

    Fix p2

--- 1-10/p02.ml
+++ 1-10/p02.ml
@@ -1,8 +1,8 @@
 let rec last_two list = match list with
         | [] | [_] -> None
         | [x;y] -> Some (x, y)
-        | h::t -> last_two t;;
+        | _::t -> last_two t;;
 
-assert (last_two [ 1, 2, 3, 4 ] = Some (3, 4));;
-assert (last_two [ `a ] = None);;
+assert (last_two [ 1; 2; 3; 4 ] = Some (3, 4));;
+assert (last_two [ 1 ] = None);;
 assert (last_two [] = None);;
